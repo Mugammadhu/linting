931c5bea542e9cf6202da75b4c464b048159e77b
Mugammadhu <mugammadhuoff@gmail.com>
2024-07-20 12:44:59 +0530

    updated code

---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginImport from 'eslint-plugin-import';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import airbnbConfig from 'eslint-config-airbnb';
+import prettierConfig from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      import: pluginImport,
+      'jsx-a11y': pluginJsxA11y,
+      prettier: pluginPrettier,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReactHooks.configs.recommended.rules,
+      ...airbnbConfig.rules,
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off', // Not needed with React 17+
+      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+      'import/prefer-default-export': 'off',
+      'no-unused-vars': 'warn',
+      'react/prop-types': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
